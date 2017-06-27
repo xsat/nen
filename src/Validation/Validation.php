8cde12d6b1e6a76b40e42935ce51fc29c9b4be33
Xsat <xsat.php@gmail.com>
2017-06-27 21:50:07 +0300

    Added validation constructor

--- src/Validation/Validation.php
+++ src/Validation/Validation.php
@@ -2,9 +2,19 @@
 
 namespace Nen\Validation;
 
+use Nen\Validation\Validator\ValidatorInterface;
+
 /**
  * Class Validation
  */
 class Validation implements ValidationInterface
 {
+    /**
+     * Validation constructor.
+     *
+     * @param ValidatorInterface[] $validators
+     */
+    public function __construct(array $validators = [])
+    {
+    }
 }
