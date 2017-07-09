55fd44eb12f2532eee08ccc1ab7b33fd3863359e
Xsat <xsat.php@gmail.com>
2017-07-09 17:07:10 +0300

    Aded PHPUnit

---
+++ tests/RequestTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class RequestTest
+ */
+class RequestTest extends TestCase
+{
+    public function testExpectTrue()
+    {
+        $this->assertTrue(1);
+    }
+
+    public function expectTrueTest()
+    {
+        $this->assertTrue(1);
+    }
+}
---
+++ tests/SomeTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class SomeTest
+ */
+class SomeTest extends TestCase
+{
+    public function testExpectTrue()
+    {
+        $this->assertTrue(1);
+    }
+
+    public function expectTrueTest()
+    {
+        $this->assertTrue(1);
+    }
+}
